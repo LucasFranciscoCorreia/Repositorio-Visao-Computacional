864700a63bf6bd85b93d3f7d123835468a822d99
lucasfranciscocorreia <lfpgc96@hotmail.com>
2020-08-29 15:39:40 -0300

    multiply

--- Exemplo_Aula_4/main.py
+++ Exemplo_Aula_4/main.py
@@ -1,10 +1,35 @@
 import cv2
+import numpy as np
 
-videocapture = cv2.VideoCapture(-1)
+def imshow_for(title):
+    videocapture = cv2.VideoCapture(-1)
+    while (True):
+        ret, frame = videocapture.read()
+        cap = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        for i in range(len(cap)):
+            for j in range(len(cap[i])):
+                if cap[i][j] * 5 > 255:
+                    cap[i][j] = 255
+                else:
+                    cap[i][j] = cap[i][j] * 5
 
-while(True):
-    ret, frame =  videocapture.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Camera", frame)
-    if(cv2.waitKey(1) == 27):
-        break
+        cv2.imshow(title, cap)
+        if cv2.waitKey(1) == 27:
+            break
+
+
+def imshow_fun(title):
+    videocapture = cv2.VideoCapture(-1)
+    while True:
+        ret, frame = videocapture.read()
+        cap = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        mask = np.ones((len(cap), len(cap[0])))*5
+        cap = cap*mask
+        cap[cap >= 255] = 255
+        cap = cap.astype(np.uint8)
+        cv2.imshow(title, cap)
+        if cv2.waitKey(1) == 27:
+            break
+
+imshow_for("for")
+imshow_fun("fun")
