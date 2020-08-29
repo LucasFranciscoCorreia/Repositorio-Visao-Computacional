614574cf8b5b31a984dfecd69b2b73f76ad88bd7
lucasfranciscocorreia <lfpgc96@hotmail.com>
2020-08-29 14:09:24 -0300

    camera funcional

--- Exemplo_Aula_4/main.py
+++ Exemplo_Aula_4/main.py
@@ -1,10 +1,10 @@
 import cv2
 
-videocapture = cv2.VideoCapture(0)
+videocapture = cv2.VideoCapture(-1)
 
 while(True):
-    capture = videocapture.read()
-    cv2.cvtColor(capture, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Camera", capture)
+    ret, frame =  videocapture.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Camera", frame)
     if(cv2.waitKey(1) == 27):
         break
