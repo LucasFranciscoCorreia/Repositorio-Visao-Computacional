b81b7c39e00cf4d11e22f72b7eb8edcb76a697c6
lucasfranciscocorreia <lfpgc96@hotmail.com>
2020-08-29 15:54:53 -0300

    update camera

--- Exemplo_Aula_4/main.py
+++ Exemplo_Aula_4/main.py
@@ -2,10 +2,10 @@ import cv2
 import numpy as np
 
 def imshow_for(title):
-    videocapture = cv2.VideoCapture(-1)
     while (True):
         ret, frame = videocapture.read()
         cap = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("original", cap)
         for i in range(len(cap)):
             for j in range(len(cap[i])):
                 if cap[i][j] * 5 > 255:
@@ -19,17 +19,19 @@ def imshow_for(title):
 
 
 def imshow_fun(title):
-    videocapture = cv2.VideoCapture(-1)
     while True:
         ret, frame = videocapture.read()
         cap = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         mask = np.ones((len(cap), len(cap[0])))*5
-        cap = cap*mask
-        cap[cap >= 255] = 255
-        cap = cap.astype(np.uint8)
-        cv2.imshow(title, cap)
+        cap2 = cap*mask
+        cap2[cap2 >= 255] = 255
+        cap2 = cap2.astype(np.uint8)
+        cv2.imshow(title, cap2)
+        cv2.imshow("original", cap)
         if cv2.waitKey(1) == 27:
             break
 
+videocapture = cv2.VideoCapture(-1)
+
 imshow_for("for")
-imshow_fun("fun")
+# imshow_fun("fun")
